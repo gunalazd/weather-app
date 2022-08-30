45cb0709f7869bf04abecccd76ad612d6af4a3a9
Gunars Lazdans <gunars.lazdans@gmail.com>
2022-08-30 12:35:50 +0200

    adding weather icon

--- src/app/current/current.component.ts
+++ src/app/current/current.component.ts
@@ -8,12 +8,20 @@ import { WeatherService } from '../services/weather.service';
 })
 export class CurrentComponent implements OnInit {
   weatherInfo: any;
+  weatherIconURL: string = '';
 
   constructor(private ws: WeatherService) {}
 
   ngOnInit(): void {
     this.ws.weatherInfo.subscribe((weatherInfo) => {
       this.weatherInfo = weatherInfo;
+      this.getWeatherIcon();
     });
+  }
+
+  getWeatherIcon() {
+    let code = this.weatherInfo.weather[0].icon;
+    this.weatherIconURL =
+      'http://openweathermap.org/img/wn/' + code + '@4x.png';
   }
 }
